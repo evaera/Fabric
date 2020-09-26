7954eb418a20270f0c411d273630da8ab63128d0
Eryn Lynn <eryn@eryn.io>
2020-09-25 22:43:32 -0400

    Remove todo

--- src/Fabric/Types.lua
+++ src/Fabric/Types.lua
@@ -8,7 +8,7 @@ Types.ComponentDefinition = t.interface({
 	reducer = t.optional(t.callback);
 	check = t.optional(t.callback);
 	defaults = t.optional(t.map(t.string, t.any));
-	components = t.optional(t.map(t.string, t.any)); -- TODO
+	components = t.optional(t.map(t.string, t.any));
 	refCheck = t.optional(t.union(t.array(t.string), t.callback));
 	shouldUpdate = t.optional(t.callback);
 
