7aaab0861329939652d79b527943a69954a28487
Eryn Lynn <eryn@eryn.io>
2020-11-11 12:32:42 -0500

    Remove confusing example

--- example/server/components/Door.lua
+++ example/server/components/Door.lua
@@ -25,16 +25,4 @@ return {
 	onUpdated = function(self)
 		print(self.ref.Name, "updated")
 	end;
-
-	effects = {
-		function(self)
-			if self.ref.Name == "Copy" then
-				local other = self.fabric:getComponentByRef("Door", self.ref.Parent.Part)
-
-				self:addLayer(self, {
-					transparency = other and other:get("transparency") or 0
-				})
-			end
-		end
-	};
 }
