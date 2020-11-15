8c238a3c351cff2fd95f42c57bc6b051b766c5b2
sam <sayhisam1@gmail.com>
2020-11-14 22:00:43 -0500

    Add test invocation via client RPC

--- clientTests/init.client.lua
+++ clientTests/init.client.lua
@@ -1,14 +1,10 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local FabricLib = require(ReplicatedStorage:WaitForChild("Fabric"))
-
 local tests = {}
 
-local fabric = FabricLib.Fabric.new("test")
-
-function tests.test_name()
-	return "some_value"
+function tests.invoke_test(...)
+	require(ReplicatedStorage.Packages.TestEZ).TestBootstrap:run(...)
+	return
 end
-
 
 ReplicatedStorage:WaitForChild("__TestRPC").OnClientInvoke = function(name, ...)
 	return tests[name](...)
