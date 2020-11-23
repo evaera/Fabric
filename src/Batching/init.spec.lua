9f2ce38e4f73ef87abfcd8230f0ffc1ebfecb0fe
sam <sayhisam1@gmail.com>
2020-11-22 23:10:07 -0500

    batching: Add `unit.getAllUnits`

--- src/Batching/init.spec.lua
+++ src/Batching/init.spec.lua
@@ -108,6 +108,22 @@ return function()
 			expect(math.abs(unit:get("testValue") - 5) <= 1).to.equal(true)
 			fabric:removeAllUnitsWithRef(ref)
 		end)
+
+		it("should statically retrieve all units", function()
+			local refs = {}
+			for i = 1, 10 do
+				refs[i] = {}
+			end
+
+			local staticUnit = makeUnitDefinition(function() end)
+			fabric:registerUnit(staticUnit)
+
+			for _, ref in ipairs(refs) do
+				fabric:getOrCreateUnitByRef(staticUnit, ref):mergeBaseLayer({})
+			end
+
+			expect(#staticUnit.getAllUnits()).to.equal(#refs)
+		end)
 	end)
 
 end
